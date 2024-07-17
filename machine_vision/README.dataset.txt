b29c5fbec58f21c759681905d91a57227984825d
msle237-lees <59542013+msle237-lees@users.noreply.github.com>
2024-07-17 21:29:05 +0000

    testing locally for a bit

---
+++ machine_vision/README.dataset.txt
@@ -0,0 +1,6 @@
+# AUVSim Prequals > 2024-07-17 2:05am
+https://universe.roboflow.com/kennesaw-state-university/auvsim-prequals
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- modules/CameraPackage.py
+++ modules/CameraPackage.py
@@ -1,12 +1,17 @@
 from ultralytics import YOLO
 import pyautogui
+import json
 import cv2
 import os
+from roboflow import Roboflow
 
 
 class CameraPackage:
-    def __init__(self):
-        self.model = YOLO("yolov8n.pt")
+    def __init__(self, apiKey):
+        rf = Roboflow(api_key=apiKey)
+        project = rf.workspace("kennesaw-state-university").project("auvsim-prequals")
+        version = project.version(2)
+        self.dataset = version.download("yolov8")
     
     def take_picture(self):
         screenshot = pyautogui.screenshot()
@@ -34,7 +39,9 @@ class CameraPackage:
     
 
 if __name__ == '__main__':
-    camera_package = CameraPackage()
+    with open("../config/api_key.json", "r") as f:
+        api_key = json.load(f)["api_key"]
+    camera_package = CameraPackage(apiKey=api_key)
     img = camera_package.take_picture()
     img, results = camera_package.predict_and_detect(img)
     cv2.imshow('img', img)
